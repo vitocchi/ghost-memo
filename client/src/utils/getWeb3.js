ba537be5185e554c05d489807589f7e490a5b897
vitocchi <vitocchi.mm@gmail.com>
2020-01-29 04:38:06 +0900

    ブラウザのproviderを最初にチェック

--- client/src/utils/getWeb3.js
+++ client/src/utils/getWeb3.js
@@ -6,11 +6,13 @@ const getWeb3 = () =>
     window.addEventListener("load", async () => {
       // Modern dapp browsers...
       if (window.ethereum) {
-          const provider = new Web3.providers.HttpProvider(
-              "http://127.0.0.1:9545"
-          );
-          const web3 = new Web3(provider);
-          resolve(web3);
+          const web3 = new Web3(window.ethereum);
+          try {
+            await window.ethereum.enable();
+            resolve(web3);
+          } catch(e) {
+            reject(e)
+          }
       }
       // Legacy dapp browsers...
       else if (window.web3) {
